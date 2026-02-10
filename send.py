2de67f24d5e0c80a738c28caa1ff63b3953bd3f0
lovesimone <simoneisatrocious@gmail.com>
2026-02-10 16:52:26 +0100

    Create send.py

---
+++ send.py
@@ -0,0 +1,16 @@
+import os
+import requests
+from datetime import datetime
+import pytz
+
+WEBHOOK_URL = os.environ["WEBHOOK_URL"]
+
+tz = pytz.timezone("Europe/Paris")
+start_date = tz.localize(datetime(2026, 2, 10))
+
+now = datetime.now(tz)
+days = (now.date() - start_date.date()).days + 1
+
+message = f"<@459679133513809951>\n<@624454617119588363> [{days}]"
+
+requests.post(WEBHOOK_URL, json={"content": message})
