4989e92b16cee1e8aa214e6ba16b3187a7321b49
lovesimone <simoneisatrocious@gmail.com>
2026-02-10 17:00:30 +0100

    Update send.py

--- send.py
+++ send.py
@@ -11,6 +11,8 @@ start_date = tz.localize(datetime(2026, 2, 10))
 now = datetime.now(tz)
 days = (now.date() - start_date.date()).days + 1
 
-message = f"<@459679133513809951>\n`<@624454617119588363> Today is [{days}] after that threat.`"
-
+message = (
+    f"<@459679133513809951>\n"
+    f"<@624454617119588363> `Today is {days} after this [threat](https://discord.com/channels/500485047338663936/1150299145563807845/1470807354554712221).`"
+)
 requests.post(WEBHOOK_URL, json={"content": message})
